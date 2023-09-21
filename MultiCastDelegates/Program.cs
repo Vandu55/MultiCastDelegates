6938f27626ba6400c30c47d0a2b7bf578a9245d6
Ankit-Kumar4 <113576375+Ankit-Kumar4@users.noreply.github.com>
2023-09-21 20:26:40 +0530

    Add project files.

---
+++ MultiCastDelegates/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiCastDelegates
+{
+
+
+    public delegate void cal(int a, int b);
+    internal class Program
+    {
+        public  static void  Addtion(int num1,int num2)
+        {
+            int result = num1 + num2; 
+            Console.WriteLine("Addtion of  two numbers:"+result);
+        }
+        public static void Multiply(int num1, int num2)
+        {
+            int result = num1 * num2;
+            Console.WriteLine("multiply of  two numbers:" + result);
+        }
+        public static void Divison(int num1, int num2)
+        {
+            int result = num1 / num2;
+            Console.WriteLine("Divison of  two numbers:" + result);
+        }
+        static void Main(string[] args)
+        {
+            cal obj = new cal(Addtion);
+            //obj(56, 67);
+            //obj = Multiply;
+            //obj(56, 7);
+            // muticasting method
+            obj += Multiply;
+            
+            obj += Divison;
+            obj(150, 100);
+            Console.WriteLine();
+
+            
+
+            
+        }
+
+    }
+}
